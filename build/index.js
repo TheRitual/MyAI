6747a09c9b8a9b69ce9ddd07da381fecd04b2114
Marcin Kawczyński <dev@theRitual.eu>
2022-10-20 08:55:26 +0200

    removed build

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const initial_system_configuration_1 = require("./data/initial-system-configuration");
-const print_system_configuration_1 = require("./utils/print-system-configuration");
-console.clear();
-console.log("\nINITIALIZATION");
-(0, print_system_configuration_1.printSystemConfiguration)(initial_system_configuration_1.INITIAL_SYSTEM_CONFIGURATION);
